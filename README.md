3104baac298bd1c4f4b1ab9b2266ed775da6e85f
Preznt <bjw1403@gmail.com>
2023-03-06 15:58:09 +0900

    naver api

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Biz_Flutter_2023_02
 
+### 3/6
+
 `flutter pub get` 해주기
+Error Lens 확장 프로그램 설치
---
+++ flutter_naver_api/android/app/src/main/kotlin/com/example/flutter_naver_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_naver_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
